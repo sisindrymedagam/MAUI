97241a4ab70ad5556a185100ccdc4283fb160b4c
Sisindry M <24821639+sisindrymedagam@users.noreply.github.com>
2025-08-17 00:52:28 +0530

    MAUI init

---
+++ YTShorts/YTShorts.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace YTShorts.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ YTShorts/YTShorts.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace YTShorts.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
